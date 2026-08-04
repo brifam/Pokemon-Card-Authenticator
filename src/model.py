13b301422a6ae2aeab7c109a0ad3eed067276a35
Brian Pham <bpham02@syr.edu>
2026-08-04 17:44:00 -0400

    Created First Model (CNN)

---
+++ src/model.py
@@ -0,0 +1,45 @@
+#Import libraries
+
+import torch
+from torch import nn
+
+
+### First Model (CNN)
+class PokemonCardModelV1(nn.Module):
+    def __init__(self,input_shape: int, hidden_units: int, output_shape: int):
+        super().__init__()
+        self.conv_block1= nn.Sequential(
+            nn.Conv2d(in_channels = input_shape, out_channels= hidden_units,kernel_size=3,padding=1, stride = 1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels = hidden_units, out_channels = hidden_units, kernel_size=3, padding = 1, stride = 1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride = 2)
+        )
+
+        self.conv_block2 = nn.Sequential(
+            nn.Conv2d(in_channels = hidden_units, out_channels= hidden_units, kernel_size= 3, padding= 1, stride=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels= hidden_units, out_channels= hidden_units, kernel_size= 3, padding=1, stride =1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size = 2, stride =2)
+        )
+
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(in_features= hidden_units*56*56, out_features= output_shape)
+        )
+
+    def forward(self,x):
+        x = self.conv_block1(x)
+        print(x.shape)
+        x = self.conv_block2(x)
+        print(x.shape)
+        x = self.classifier(x)
+        print(x.shape)
+
+        return x
+
+
+
+
+
